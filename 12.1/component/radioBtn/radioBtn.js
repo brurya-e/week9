17f9c07ddedc25cfaf03d0aecd6cff899f9c2fb3
brurya-e <brurya.zak@gmail.com>
2021-12-19 13:00:23 +0200

    12.1

---
+++ 12.1/component/radioBtn/radioBtn.js
@@ -0,0 +1,18 @@
+import "./radioBtn.css"
+import React from "react";
+
+const RadioBtn = ({ text,checkedFlage,onChange, key}) => {
+    return (
+        <div>
+            <input
+                type={"radio"}
+                name={text}
+                checked ={checkedFlage}
+                onChange ={()=>{onChange(text)}}
+            />
+            <label for={text}>{text}</label>
+        </div >
+    );
+};
+
+export default RadioBtn;
